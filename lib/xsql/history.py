666643c241b3a66d1efea40fde56f4751f5048f7
Ryan Kelly <ryan@ryankelly.us>
2025-06-19 22:28:08 +0000

    implement \e command

--- lib/xsql/history.py
+++ lib/xsql/history.py
@@ -1,4 +1,5 @@
 import itertools
+import os
 
 from prompt_toolkit.history import FileHistory as BaseFileHistory
 
@@ -9,3 +10,6 @@ class FileHistory(BaseFileHistory):
 
     def load_history_strings(self):
         return itertools.islice(super().load_history_strings(), config.history_size)
+
+
+history = FileHistory(os.path.expanduser("~/.xsql_history"))
