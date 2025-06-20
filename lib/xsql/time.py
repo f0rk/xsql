49ea9a943d7f89f7ace052e7bb3d8005e121512e
Ryan Kelly <ryan@ryankelly.us>
2025-06-20 08:19:20 +0000

    ruff

--- lib/xsql/time.py
+++ lib/xsql/time.py
@@ -6,14 +6,14 @@ def write_time(total_time):
     formatted_ms = "{:.3f}".format(Decimal(total_time) / Decimal("100000"))
 
     if "." in formatted_ms:
-        l, r = formatted_ms.split(".")
+        left, right = formatted_ms.split(".")
     else:
-        l = formatted_ms
-        r = "0"
+        left = formatted_ms
+        right = "0"
 
-    while len(r) < 3:
-        r = r + "0"
+    while len(right) < 3:
+        right = right + "0"
 
-    formatted_ms = l + "." + r
+    formatted_ms = left + "." + right
 
     sys.stdout.write("Time: {} ms\n".format(formatted_ms))
